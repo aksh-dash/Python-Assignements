8d42cce61b73ed649f9f71b4aed6e76faacd2523
aksh-dash <dange.akshat2107@gmail.com>
2025-06-11 01:22:33 +0530

    python assignment

---
+++ ASSIGNMENT 1.1.py
@@ -0,0 +1,13 @@
+first=int(input("Enter first number: "))
+second=int(input("Enter second number: "))
+
+#operations
+addition=first+second
+subtraction=first-second
+multiplication=first*second
+division=first/second
+
+print("Addition =",addition)
+print("Subtraction =",subtraction)
+print("Multiplication =",multiplication)
+print("Division =",division)
---
+++ Assignment 1.2.py
@@ -0,0 +1,4 @@
+firstname=input("Enter your first name: ")
+lastname=input("Enter your last name: ")
+
+print("Welcome",firstname,lastname,"to the world of Python programming")
